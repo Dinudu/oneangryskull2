81149dead3a51d36044ace5c58ea475f99f1db5e
webscept_solutions <accounts@webscepts.com>
2023-12-25 13:25:34 +0530

    changeup

--- woocommerce/single-product/product-image.php
+++ woocommerce/single-product/product-image.php
@@ -42,7 +42,6 @@ $wrapper_classes   = apply_filters(
 		if ( $post_thumbnail_id ) {
 			$html = wc_get_gallery_image_html( $post_thumbnail_id, true );
 		} else {
-			$html  = '<div class="woocommerce-product-gallery__image--placeholder">';
 			$html .= sprintf( '<img src="%s" alt="%s" class="wp-post-image" />', esc_url( wc_placeholder_img_src( 'woocommerce_single' ) ), esc_html__( 'Awaiting product image', 'woocommerce' ) );
 			$html .= '</div>';
 		}
