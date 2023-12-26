c6d71579796f2933074ef9d35a57e9bf0983ee89
webscept_solutions <accounts@webscepts.com>
2023-12-26 11:48:34 +0530

    shop page

--- woocommerce/content-product.php
+++ woocommerce/content-product.php
@@ -25,9 +25,32 @@ if ( empty( $product ) || ! $product->is_visible() ) {
 }
 ?>
 <div class="col-xs-6 col-sm-4 col-lg-4">
-<article class="bottom">
-<a href="<?php the_permalink(); ?>" class="my-custom-product-link woocommerce-loop-product__link">
-    <?php the_post_thumbnail('shop_catalog'); ?>
-</a>
 <article>
-</div>
+                                <div class="info">
+                                    <span class="add-favorite">
+                                        <a href="javascript:void(0);" data-title="Add to favorites" data-title-added="Added to favorites list"><i class="icon icon-heart"></i></a>
+                                    </span>
+                                    <span>
+                                        <a href="<?php the_permalink(); ?>" class="mfp-open" data-title="Quick wiew"><i class="icon icon-eye"></i></a>
+                                    </span>
+                                </div>
+                                <div class="btn btn-add">
+                                    <i class="icon icon-cart"></i>
+                                </div>
+                                <div class="figure-grid">
+                                    <div class="image">
+                                        <a href="#productid1" class="mfp-open">
+                                            <img src="<?php the_post_thumbnail('shop_catalog'); ?>" alt="" width="360">
+                                        </a>
+                                    </div>
+                                    <div class="text">
+                                        <h2 class="title h4">
+                                            <a href="product.html">Coffee machine</a>
+                                        </h2>
+                                        <sub>$ 159,-</sub>
+                                        <sup>$ 139,-</sup>
+                                        <span class="description clearfix">Gubergren amet dolor ea diam takimata consetetur facilisis blandit et aliquyam lorem ea duo labore diam sit et consetetur nulla</span>
+                                    </div>
+                                </div>
+    </article>
+
