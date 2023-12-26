3139556e66f4adcba41b5d7b8566ff2654ac7416
webscept_solutions <accounts@webscepts.com>
2023-12-26 11:43:23 +0530

    shop page

--- woocommerce/content-product.php
+++ woocommerce/content-product.php
@@ -26,44 +26,8 @@ if ( empty( $product ) || ! $product->is_visible() ) {
 ?>
 <div class="col-xs-6 col-sm-4 col-lg-4">
 <article class="bottom">
-	<?php
-	/**
-	 * Hook: woocommerce_before_shop_loop_item.
-	 *
-	 * @hooked woocommerce_template_loop_product_link_open - 10
-	 */
-	do_action( 'woocommerce_before_shop_loop_item' );
-
-	/**
-	 * Hook: woocommerce_before_shop_loop_item_title.
-	 *
-	 * @hooked woocommerce_show_product_loop_sale_flash - 10
-	 * @hooked woocommerce_template_loop_product_thumbnail - 10
-	 */
-	do_action( 'woocommerce_before_shop_loop_item_title' );
-
-	/**
-	 * Hook: woocommerce_shop_loop_item_title.
-	 *
-	 * @hooked woocommerce_template_loop_product_title - 10
-	 */
-	do_action( 'woocommerce_shop_loop_item_title' );
-
-	/**
-	 * Hook: woocommerce_after_shop_loop_item_title.
-	 *
-	 * @hooked woocommerce_template_loop_rating - 5
-	 * @hooked woocommerce_template_loop_price - 10
-	 */
-	do_action( 'woocommerce_after_shop_loop_item_title' );
-
-	/**
-	 * Hook: woocommerce_after_shop_loop_item.
-	 *
-	 * @hooked woocommerce_template_loop_product_link_close - 5
-	 * @hooked woocommerce_template_loop_add_to_cart - 10
-	 */
-	do_action( 'woocommerce_after_shop_loop_item' );
-	?>
+<a href="<?php the_permalink(); ?>" class="my-custom-product-link woocommerce-loop-product__link">
+    <?php the_post_thumbnail('shop_catalog'); ?>
+</a>
 <article>
 </div>
