bd975cc3e0aab78174f709df5e44a0f32f51f111
webscept_solutions <accounts@webscepts.com>
2024-01-14 15:20:10 +0530

    newcheckout

--- checkout.php
+++ checkout.php
@@ -131,7 +131,71 @@ get_header();
                                             <div class="h4">Register now <a href="javascript:void(0);" class="btn btn-main btn-xs btn-login pull-right">Log in</a></div>
 
                                             <hr />
-                                          <?php echo do_shortcode('[create_account_form]'); ?>
+                                        <form method="POST">
+                                            <div class="row">
+
+                                                <div class="col-md-6">
+                                                    <div class="form-group">
+                                                        <input type="text" value="" class="form-control" placeholder="First name: *">
+                                                    </div>
+                                                </div>
+
+                                                <div class="col-md-6">
+                                                    <div class="form-group">
+                                                        <input type="text" value="" class="form-control" placeholder="Last name: *">
+                                                    </div>
+                                                </div>
+
+                                                <div class="col-md-12">
+
+                                                    <div class="form-group">
+                                                        <input type="text" value="" class="form-control" placeholder="Company name:">
+                                                    </div>
+                                                </div>
+
+                                                <div class="col-md-4">
+                                                    <div class="form-group">
+                                                        <input type="text" value="" class="form-control" placeholder="Zip code: *">
+                                                    </div>
+                                                </div>
+
+                                                <div class="col-md-8">
+                                                    <div class="form-group">
+                                                        <input type="text" value="" class="form-control" placeholder="City: *">
+                                                    </div>
+                                                </div>
+
+                                                <div class="col-md-6">
+                                                    <div class="form-group">
+                                                        <input type="text" value="" class="form-control" placeholder="Email: *">
+                                                    </div>
+                                                </div>
+
+                                                <div class="col-md-6">
+                                                    <div class="form-group">
+                                                        <input type="text" value="" class="form-control" placeholder="Phone: *">
+                                                    </div>
+                                                </div>
+
+                                                <div class="col-md-12">
+                                                    <hr />
+                                                    <span class="checkbox">
+                                                        <input type="checkbox" id="checkBoxId1">
+                                                        <label for="checkBoxId1">I have read and accepted the <a href="#">terms</a>, as well as read and understood our terms of <a href="#">business contidions</a></label>
+                                                    </span>
+                                                    <span class="checkbox">
+                                                        <input type="checkbox" id="checkBoxId2">
+                                                        <label for="checkBoxId2">Subscribe to exciting newsletters and great tips</label>
+                                                    </span>
+                                                    <hr />
+                                                </div>
+
+                                                <div class="col-md-12">
+                                                    <a href="#" class="btn btn-main btn-block">Create account</a>
+                                                </div>
+
+                                            </div>
+                                        </form>
 
                                         </div> <!--/signup-->
                                     </div>
