87e5aef1b6702502d64ffe92ac9e5de1a6b028b1
webscept_solutions <accounts@webscepts.com>
2024-01-14 17:53:01 +0530

    checkout

--- checkout.php
+++ checkout.php
@@ -416,6 +416,8 @@ get_header();
                         <div class="col-xs-6 text-right">
                             <a href="checkout-3.html" class="btn btn-main"><span class="icon icon-cart"></span> Go to payment</a>
                         </div>
+                        <?php echo apply_filters( 'woocommerce_order_button_html', '<button type="submit" class="button alt" name="woocommerce_checkout_place_order" id="place_order" value="' . esc_attr( $order_button_text ) . '" data-value="' . esc_attr( $order_button_text ) . '">' . esc_html( $order_button_text ) . '</button>' ); ?>
+
                     </div>
                 </div>
 
